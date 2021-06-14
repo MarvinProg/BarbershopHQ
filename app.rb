8a64b8ff854ae92a6590f83664d79a0d1ea38051
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-14 20:13:29 +0300

    Display error on failed validation

--- app.rb
+++ app.rb
@@ -32,7 +32,11 @@ end
 post '/visit' do
 
 c = Client.new params[:client]
-c.save
+if c.save 
+  erb "<h2>Спасибо, вы записались!</h2>"
+else
+  erb "<h2>Ошибка</h2>"
+end
 
 =begin  
   @username = params[:username]
